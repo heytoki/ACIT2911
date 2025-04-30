fdaf06b316121d8908e98d2e806a107b06f66d88
MuhammadBai <baigm9548@gmail.com>
2025-04-30 15:20:26 -0700

    db

--- models.py
+++ models.py
@@ -1,2 +1,18 @@
-# This will be the tables inside of our database
-# We can decide what data we want to store for each recipe
+# models.py
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import Integer, String, Text
+
+class Base(DeclarativeBase):
+    pass
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = mapped_column(Integer, primary_key=True)
+    title = mapped_column(String, nullable=False)
+    ingredients = mapped_column(Text, nullable=False)
+    instructions = mapped_column(Text)
+    category = mapped_column(String)
+
+    def __repr__(self):
+        return f"<Recipe {self.title}>"
