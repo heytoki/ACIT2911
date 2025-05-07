542574b2bdf1db01b1bc7586a034d7b4f6c678fb
heytoki <hitoki.nakajo@gmail.com>
2025-05-06 18:25:09 -0700

    ingredient.html backend

--- app.py
+++ app.py
@@ -31,11 +31,10 @@ def list():
 
 @app.route('/recipes/create', methods=['GET', 'POST'])
 def create():
-    ingredientsList = Ingredient.query.order_by(asc(Ingredient.name)).all()
     if request.method == 'POST':
-        title = request.form.get('title')
-        ingredients_text = request.form.get('ingredientsForm')  # multiple lines
-        instructions_text = request.form.get('instructionsForm')  # optional
+        title = request.form.get('recipeName')
+        ingredients_text = request.form.getlist('ingredientNames')
+        instructions_text = request.form.getlist('instructionsList')
 
         # Create recipe
         new_recipe = Recipe(title=title, instructions=instructions_text)
@@ -43,10 +42,10 @@ def create():
         db.session.commit()
 
         # Create ingredient entries
-        ingredients = ingredients_text.strip().split('\n')
-        for item in ingredients:
-            if item.strip():
-                db.session.add(Ingredient(name=item.strip(), recipe_id=new_recipe.id))
+        # ingredients = ingredients_text.strip().split('\n')
+        # for item in ingredients:
+        #     if item.strip():
+        #         db.session.add(Ingredient(name=item.strip(), recipe_id=new_recipe.id))
 
         db.session.commit()
 
@@ -54,16 +53,27 @@ def create():
         save_instructions_to_json(new_recipe.id, instructions_text)
 
         return redirect(url_for('home'))
-
-    return render_template('create.html', ingredientsList=ingredientsList)
+    
+    if request.method == 'GET':
+        ingredientsList = Ingredient.query.order_by(asc(Ingredient.name)).all()
+        return render_template('create.html', ingredientsList=ingredientsList)
 
 @app.route('/recipes/recipe')
 def recipe():
     return render_template('recipe.html')
 
-@app.route('/ingredient')
+@app.route('/ingredient', methods=['GET', 'POST'])
 def ingredient():
-    return render_template('ingredient.html')
+    if request.method == 'GET':
+        return render_template('ingredient.html')
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        measure = request.form.to_dict()['measures']
+        ingredient = Ingredient(name=name, measure=measure)
+        db.session.add(ingredient)
+        db.session.commit()
+        return render_template('ingredient.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
--- models.py
+++ models.py
@@ -7,10 +7,10 @@ class Recipe(db.Model):
     cuisine = db.Column(db.String(50))
     cook_time = db.Column(db.String)
     difficulty = db.Column(db.String(20))
-
     ingredients = db.relationship("Ingredient", backref="recipe", lazy=True)
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    measure = db.Column(db.String())
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
